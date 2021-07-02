c452670186a5d5892e955b6d5c7f09db731ae2a3
Aniko B Toth <abtoth2@gmail.com>
2021-07-02 17:46:38 +1000

    Clean code, remove old stuff no longer in use, update object names, replace beta diversity and composition analyses

--- Code/Analysis_Script.R
+++ Code/Analysis_Script.R
@@ -40,18 +40,16 @@ chao %>% group_by(taxon, status) %>% summarise(mean.rich = mean(richness, na.rm 
 #significance test
 chao %>% group_by(taxon) %>% summarise(w=wilcox.test(richness~status, paired=FALSE)$p.value)
 
-#### Change to presence-absence ####
-PAn<- tobinary(PAn)
+
+#### Match Biogeography ####
 PAnu <- PAn %>% map(~return(.[,colnames(.) %in% unalt_sites])) # separate unaltered
 PAna <- PAn %>% map(~return(.[,!colnames(.) %in% unalt_sites])) # and altered sites
 
-#### Match Biogeography ####
-meta <- sitedat %>% split(.$taxon)
-coords <- map(meta, select, c(sample.no, latitude, longitude)) 
+coords <- sitedat %>% select(taxon, p.sample, latitude, longitude) %>% distinct() %>% split(.$taxon)
 
 # Remove unaltered or altered sites that are not near a site of the other type. This is done to ensure the two sets have similar biogoegraphical distributions.
-coords1 <- purrr::map2(coords, PAnu, function(x, y) x[x$sample.no %in% colnames(y),])
-coords2 <- purrr::map2(coords, PAna, function(x, y) x[x$sample.no %in% colnames(y),])
+coords1 <- purrr::map2(coords, PAnu, function(x, y) x[x$p.sample %in% colnames(y),])
+coords2 <- purrr::map2(coords, PAna, function(x, y) x[x$p.sample %in% colnames(y),])
 keep <- map2(coords1, coords2, matchbiogeo) %>% map(unlist)
 
 PAn <- map2(PAn, keep, function(x, y) return(x[,as.character(y)]))
@@ -62,21 +60,32 @@ PAnu <- PAn %>% map(~return(.[,colnames(.) %in% unalt_sites]))
 PAna <- PAn %>% map(~return(.[,!colnames(.) %in% unalt_sites]))
 
 # coords of sites we kept, for plotting later.
-coords1.keep <- purrr::map2(coords, PAnu, function(x, y) x[x$sample.no %in% colnames(y),])
-coords2.keep <- purrr::map2(coords, PAna, function(x, y) x[x$sample.no %in% colnames(y),])
+coords1.keep <- purrr::map2(coords, PAnu, function(x, y) x[x$p.sample %in% colnames(y),])
+coords2.keep <- purrr::map2(coords, PAna, function(x, y) x[x$p.sample %in% colnames(y),])
 
-### Beta - run using presence-absence, biogeo matched data ####
 
-beta <- beta.types(PAn, unalt_sites)
+#### Beta diversity and composition analyses #####
+PAnb<- tobinary(PAn)
 
-# summary stats
-b <- beta %>% group_by(taxon, unalt.pair) %>% summarise(mean = mean(Z.Score), median = median(Z.Score))
-# p - values
-beta %>% dplyr::filter(unalt.pair != "Unaltered-Altered") %>% 
-  mutate(similarity = log(Z.Score)) %>% split(.$taxon) %>% 
-  purrr::map(~wilcox.test(data = ., similarity~unalt.pair, paired=FALSE))
+PA <- PAn  ## can run analyses with binary or abudance data
 
-#occupancy calculations (used later)
+# with bray-curtis index
+dist <- map(PA, ~t(.)) %>% map(vegdist, method = "bray")
+# with Ochiai index
+dist <- map(PA, ochiaiMatrix) %>% map(as.dist, upper = F) %>% map(~return(1-.))
+
+# beta diversity using betadisper
+beta <- map2(dist, PAn, function(x, y) betadisper(x, group = colnames(y) %in% unalt_sites) %>% anova)
+
+# compositional change using adonis
+data <- map(PA, ~t(.x) %>% data.frame() %>% rownames_to_column("p.sample") %>% mutate(ID = colnames(.x) %in% unalt_sites))
+comp1 <- map2(dist, data, function(x, y) adonis(x~ID, data = y, permutations = 10000))
+
+# compositional change using canonical correspondence analysis
+dat <- map(data, select, contains("_"))
+comp2 <- map2(dat, data, function(x, y) cca(x~ID, data = y) %>% anova.cca)
+
+##### occupancy calculations (used later) ######
 occ.count <- map(PAn, t) %>% map(data.frame) %>% map(~split(., rownames(.) %in% unalt_sites)) %>% 
   map(map, ~colSums(.)) %>% map(map, data.frame) %>% map(~merge(.[[1]], .[[2]], by = 0)) %>% map(setNames, c("name", "altered", "unaltered")) %>% bind_rows(.id = "taxon")
 occ.count$name <- stri_replace_all_regex(occ.count$name, "[.]", " ")
@@ -95,50 +104,24 @@ occ %>% group_by(taxon) %>% summarise(
   shared = length(which(altered > 0 & unaltered > 0)), 
   total = length(altered))
 
-#### Composition - run using presence-absence, biogeo matched data ####
-
-dist <- map(PAn, ochiaiMatrix) %>% map(as.dist, upper = F) %>% map(~return(1-.)) 
-ord <- map(dist, cmdscale, k = 2) %>% map(data.frame)
-ord <- map(ord, ~merge(., y = sitedat, all.x = T, all.y = F, by.x = 0, by.y = "sample.no"))
-
-w <- numeric()
-for(j in 1:length(ord)){
-  i <- ord[[j]]
-  d <- numeric()
-  for(r in 1:1000){
-    i$rand = round(runif(n = nrow(i), min = 0, max = 1), 0)
-    cent <- i %>% group_by(rand) %>% dplyr::summarise(x = mean(X1), y = mean(X2))
-    d[r] <- spDists(as.matrix(cent[,c(2:3)]))[2]
-  }
-  cent <- i %>% group_by(status) %>% dplyr::summarise(x = mean(X1), y = mean(X2))
-  obs <- spDists(as.matrix(cent[,c(2:3)]))[2]
-  w[j] <- wilcox.test(d, obs)$p.value
-}
-
-# P-value of PCoA centroid shift
-w
-
-
 #### Format Data for Co-occurrence analysis #####
-  # Remove empty rows from altered and unaltered tables
-PAna <- map(PAna, clean.empty) 
-PAnu <- map(PAnu, clean.empty)
 
-# no singletons
-PAn.ns <- map(PAn, clean.empty, minrow = 2)
+# remove singletons
+#PAn.ns <- map(PAn, clean.empty, minrow = 2)
 
 # Format data (full)
 tables <- PAn %>% map(~t(.)) %>% map(as.data.frame) %>% map(~split(., f = rownames(.) %in% unalt_sites)) %>% 
   map(map, ~t(.)) %>% map(map, clean.empty) %>% purrr::map(setNames, c("altered", "unaltered"))
 # shared (common-only)
-shared_bats <- rownames(tables[[1]][[1]])[which(rownames(tables[[1]][[1]]) %in% rownames(tables[[1]][[2]]))]
-shared_birds <- rownames(tables[[2]][[1]])[which(rownames(tables[[2]][[1]]) %in% rownames(tables[[2]][[2]]))]
-tables <- map2(tables, list(shared_bats, shared_birds), function(x, y) map(x, function(z) return(z[y,])))
+ shared_bats <- rownames(tables[[1]][[1]])[which(rownames(tables[[1]][[1]]) %in% rownames(tables[[1]][[2]]))]
+ shared_birds <- rownames(tables[[2]][[1]])[which(rownames(tables[[2]][[1]]) %in% rownames(tables[[2]][[2]]))]
+ tables <- map2(tables, list(shared_bats, shared_birds), function(x, y) map(x, function(z) return(z[y,])))
 
 # Contingency table
 contables <- map(tables, map, cont_table) %>% map(bind_rows, .id = "status") %>% 
   bind_rows(.id = "taxon") %>% diet_cat(spp, related = TRUE) %>% na.omit()
 
+source('./Code/omega.R', echo=TRUE)
 # Calculate omega 
 ## interaction, no bagging, no related, clipped to median
 out_bat <- omega(contables, tax = "bat", type = "interaction") 
@@ -160,125 +143,6 @@ out_bird_bag <- omega(contables, tax = "bird", type = "interaction", bagging = T
 ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch)) + 
   geom_density() + facet_wrap(parameter~., scales = "free_x")
 
-##### Functional FETmP analysis ####
-tables <- PAn.ns %>% map(~t(.)) %>% map(as.data.frame) %>% map(~split(., f = rownames(.) %in% unalt_sites)) %>% 
-  map(map, ~t(.)) %>% map(map, clean.empty) %>% purrr::map(setNames, c("altered", "unaltered"))
-
-# observed
-obs <- purrr::map(tables, ~purrr::map(., function(x) simpairs(x) %>% dist2edgelist(x))) %>%
-  purrr::map(bind_rows, .id = 'status') %>% bind_rows(.id = "taxon") %>% mutate(Taxon_status = paste(taxon, status, sep = "_"))
-
-# expected
-ss1 <- map(tables, map_int, ncol)  
-rps <- 100
-
-ss.bat <- lapply(1:rps, function(x) sample(c(rep(1, ss1$bat["altered"]), rep(2, ss1$bat["unaltered"]))))
-ss.bird <- lapply(1:rps, function(x) sample(c(rep(1, ss1$bird["altered"]), rep(2, ss1$bird["unaltered"]))))
-
-# no replacement site subsampling
-bat.a <- lapply(ss.bat, function(x) PAn.ns[[1]][,which(x == 1)]) %>% lapply(clean.empty)
-bat.u <- lapply(ss.bat, function(x) PAn.ns[[1]][,which(x == 2)]) %>% lapply(clean.empty)
-bird.a <- lapply(ss.bird, function(x) PAn.ns[[2]][,which(x == 1)]) %>% lapply(clean.empty)
-bird.u <- lapply(ss.bird, function(x) PAn.ns[[2]][,which(x == 2)]) %>% lapply(clean.empty)
-
-
-input <- list(bat.a, bat.u, bird.a, bird.u) %>% setNames(c("bat_altered", "bat_unaltered", "bird_altered", "bird_unaltered"))
-
-  ## CAUTION: The following code is the rate-limiting step when there are many species in the input tables (e.g. more than ~150).
-  ## CAUTION: The following code will also hit the RAM limit on most computers.
-  ## See below for two alternative ways to explore the results.
-    # out <- map(y, map, map, simpairs)  # FETmP calculations
-  
-  ## The rate-limiting step can be parallelized on machines with multiple cores ##     
-     cl <- makeCluster(detectCores()) 
-     clusterExport(cl, c("simpairs", "dist2edgelist"))
-     clusterEvalQ(cl, library(tidyverse))
-  ## 
-    
-   exp <- purrr::map(input, ~parLapply(cl, ., fun = function(x) simpairs(x) %>% dist2edgelist(x)) %>% 
-      bind_rows(.id = "subsample")) %>% bind_rows(.id = "Taxon_status")
-   exp$taxon <- word(exp$Taxon_status, 1, 1, sep = "_")
-   exp$status <- word(exp$Taxon_status, 2, 2, sep = "_")
-   
-   out <- bind_rows(list(expected = exp, observed = obs), .id = "type")
-  
-#### Add categorical data #####
-  out <- diet_cat(out, spp, related = FALSE)
-   
-  out$cat.Sp1[out$Sp1 %in% uniquesp] <- "Unique"
-  out$cat.Sp1[out$Sp1 %in% sharedsp] <- "Shared"
-  out$cat.Sp2[out$Sp2 %in% uniquesp] <- "Unique"
-  out$cat.Sp2[out$Sp2 %in% sharedsp] <- "Shared"
-  
-  out$cat.pair <- paste(out$cat.Sp1, out$cat.Sp2, sep = "-")
-  out$cat.pair[out$cat.pair == "Shared-Unique"] <- "Unique-Shared"
-  
-  out$cat.group[out$cat.pair == "Unique-Unique"] <- "Unique"
-  out$cat.group[out$cat.pair == "Unique-Shared"] <- "Unique"
-  out$cat.group[out$cat.pair == "Shared-Shared"] <- "Shared"
-  
-### Cosmopolitan/restricted groupings #  
-  out$cosmo.Sp1 <- cosmo[out$Sp1,"group_abbr"]
-  out$cosmo.Sp2 <- cosmo[out$Sp2,"group_abbr"]
-  out$cosmo.pair <- paste(out$cosmo.Sp1, out$cosmo.Sp2, sep = "-")
-  
-  out$cosmo.pair[out$cosmo.pair == "cosmo-restr"] <- "restr-cosmo"
-  out$cosmo.pair[out$cosmo.pair == "synan-cosmo"] <- "cosmo-synan"
-  out$cosmo.pair[out$cosmo.pair == "synan-restr"] <- "restr-synan"
-  
-#### Results collated as summary tables from output ####  
-  
-# d1: overall [NOT PRESENTED IN MANUSCRIPT] #####
-  d1.prop.all <- out %>% group_by(subsample, Taxon_status, type) %>% 
-    summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), sd = sd(Z.Score), count = length(Z.Score))
-  d1.prop.cat <- out %>% group_by(subsample, Taxon_status, cat.group, type) %>% 
-    summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), sd = sd(Z.Score), count = length(Z.Score))
-  d1.mag.all <- out %>% group_by(subsample, Taxon_status, pnz = posnegzero(Z.Score), type) %>% 
-    summarise(`Mean magnitude` = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-  d1.mag.cat <- out %>% group_by(subsample, Taxon_status, cat.group, pnz = posnegzero(Z.Score), type) %>% 
-    summarise(`Mean magnitude` = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-  
-# d2: Diet.match (competing and non-competing pairs)  ####
-  # MAGNITUDES [PRESENTED IN MAIN TEXT]
-    # magnitude of agg & seg, overall by diet group 
-    d2.mag.all <- out %>% group_by(subsample, Taxon_status, diet.match, pnz = posnegzero(Z.Score), type) %>% 
-      summarise(avmag = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-    
-    # magnitude of agg & seg, by diet group and shared/unique    
-    d2.mag.catp <- out %>% group_by(subsample, Taxon_status, diet.match, cat.pair, pnz = posnegzero(Z.Score), type) %>% 
-      summarise(avmag = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-    
-    # magnitude of agg & seg, by diet group and synanthropic/cosmpolitan/restricted  
-    d5.mag.catp <- out %>% group_by(subsample, Taxon_status, diet.match, cosmo.pair, pnz = posnegzero(Z.Score), type) %>% 
-      summarise(avmag = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-    
-   # PROPORTIONS [PRESENTED IN SUPPLEMENT]  
-    # proportion of agg:seg, overall by diet group
-    d2.prop.all <- out %>% group_by(subsample, Taxon_status, diet.match, type) %>% 
-      summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), count = length(Z.Score))
-    
-    # proportion of agg:seg, by diet group and shared/unique
-    d2.prop.catp <- out %>% group_by(subsample, Taxon_status, diet.match, cat.pair, type) %>% 
-      summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), count = length(Z.Score))
-    
-    # proportion of agg:seg, by diet group and synanthropic/cosmopolitan/restricted
-    d5.prop.catp <- out %>% group_by(subsample, Taxon_status, diet.match, cosmo.pair, type) %>% 
-      summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), count = length(Z.Score))
-    
-          
-# d3: Within-guild analysis: Diet.pair == "Same" [NOT PRESENTED IN MANUSCRIPT] ####
-  d3.mag.all <- out[out$diet.match == "Same",]  %>% group_by(subsample, Taxon_status, diet.pair, diet.match, type, pnz = posnegzero(Z.Score)) %>% 
-    summarise(avmag = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-  d3.mag.catp <- out[out$diet.match == "Same",]  %>% group_by(subsample, Taxon_status, diet.pair, diet.match, type, pnz = posnegzero(Z.Score), cat.pair) %>% 
-    summarise(avmag = mean(Z.Score), count = length(Z.Score)) %>% filter(!pnz == "ZERO")
-    
-  d3.prop.all <- out[out$diet.match == "Same",] %>% group_by(subsample, Taxon_status, diet.pair, diet.match, type) %>% 
-    summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), count = length(Z.Score))
-  d3.prop.catp <- out[out$diet.match == "Same",] %>% group_by(subsample, Taxon_status, diet.pair, diet.match, type, cat.pair) %>% 
-    summarise(seg = percneg(Z.Score), agg = percpos(Z.Score), count = length(Z.Score))
-  
-  
-  
   
 #### Analysis of co-occurrence at altered habitats ####
   # bats
