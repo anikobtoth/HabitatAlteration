0e00f2f891042a9a1a631e01f546467271b9b00b
Aniko B Toth <abtoth2@gmail.com>
2024-07-22 15:06:41 +1000

    add code for current manuscript figures and delete old code

--- Code/Figures.R
+++ Code/Figures.R
@@ -14,20 +14,70 @@ library(reshape2)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 ##  MAIN TEXT 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# bird and bat theta posterior mean and CI calculations ----
+
+# calculate posterior slope (a) and intercept (b) for each of 4 groups
+bird_post <- as.data.frame(bird_winner)
+# slopes 
+x <- data.frame(iteration = 1:nrow(bird_post), 
+                slope_control_altered = bird_post$`b_D_cut`, 
+                slope_control_intact = bird_post$`b_D_cut`,
+                slope_intraguild_altered = bird_post$`b_D_cut` + bird_post$`b_D_cut:diet.matchsame`, 
+                slope_intraguild_intact = bird_post$`b_D_cut` + bird_post$`b_D_cut:diet.matchsame`, 
+                # intercepts 
+                intercept_control_altered = bird_post$`b_Intercept`,
+                intercept_control_intact = bird_post$`b_Intercept` + bird_post$`b_statusunaltered`,
+                intercept_intraguild_altered = bird_post$`b_Intercept` + bird_post$`b_diet.matchsame`,
+                intercept_intraguild_intact = bird_post$`b_Intercept` + bird_post$`b_diet.matchsame` + bird_post$`b_statusunaltered`) %>%
+  pivot_longer(contains("_")) %>% 
+  separate(name, into = c("variable", "diet.match", "status"), sep = "_") %>% 
+  pivot_wider(names_from = "variable", values_from = "value")
+
+CI_df_bird <-seq(range(bird_data_plot$D)[1], range(bird_data_plot$D)[2], length.out = 35) %>%
+  map(~x %>% mutate(D = .x, theta = slope*D+intercept)) %>%
+  bind_rows() %>%
+  group_by(diet.match, status, D) %>%
+  summarise(theta_025 = quantile(theta, 0.025),
+            theta_975 = quantile(theta, 0.975),
+            theta_mu  = mean(theta)) %>%
+  full_join(bird_data_plot %>% group_by(diet.match, status) %>% 
+              summarise(d025 = quantile(D, 0.025), d975 = quantile(D, 0.975))) %>%
+  mutate(D95q = D > d025 & D < d975)
+
+
+bat_post <- as.data.frame(bat_winner)  
+x <- data.frame(iteration = 1:nrow(bat_post), 
+                slope= bat_post$`b_D_cut`, 
+                intercept = bat_post$`b_Intercept`)
+
+CI_df_bat <-seq(range(bat_data_plot$D)[1], range(bat_data_plot$D)[2], length.out = 35) %>%
+  map(~x %>% mutate(D = .x, theta = slope*D+intercept)) %>%
+  bind_rows() %>%
+  group_by(D) %>%
+  summarise(theta_025 = quantile(theta, 0.025),
+            theta_975 = quantile(theta, 0.975),
+            theta_mu  = mean(theta)) %>%
+  cross_join(bat_data_plot %>% group_by(diet.match, status) %>% 
+               summarise(d025 = quantile(D, 0.025), d975 = quantile(D, 0.975))) %>%
+  mutate(D95q = D > d025 & D < d975)
+# end mean and CI calculations ----
+
+colors <- c("#ADADAD", "#525252", "#2F99DC", "#00487C")
+
+## BAT PLOT FIG 2 ####
 tax = "bat"
 data_plot <- stan_data_fun(filter(contables, taxon == tax), medn = FALSE)[[2]]
 bat_data_plot <- as.data.frame(bat_winner) %>% colMeans() %>% 
   data.frame(names(.)) %>% setNames(c("value", "col")) %>% 
   filter(grepl('theta', col)) %>% separate(col, into=c("name", "index"), sep = 8) %>% 
-  pivot_wider() %>% cbind(bat_data) %>% full_join(bat_data_plot) %>% 
+  pivot_wider() %>% cbind(bat_data) %>% full_join(data_plot) %>% 
   mutate(diet.match = recode(diet.match, "different" = "control", "same" = "intraguild"), 
          status = recode(status, "unaltered" = "intact"))
 
-colors <- c("#ADADAD", "#525252", "#2F99DC", "#00487C")
 # bat plot
 bat_data_plot %>% ggplot(aes(x = D, y = theta_vl, col = interaction(status, diet.match))) + 
   geom_jitter(alpha = 0.3, width = 0.1) + 
-  #geom_hex(aes(fill = stat(count))) +
   geom_ribbon(data = filter(CI_df_bat, !D95q), aes(x = D, ymin = theta_025, ymax = theta_975), inherit.aes = FALSE, fill = "gray50", alpha = 0.25) +
   geom_ribbon(data = filter(CI_df_bat, D95q), aes(x = D, ymin = theta_025, ymax = theta_975), fill = "yellow", alpha = 0.4, inherit.aes = FALSE) +
   geom_abline(slope = -0.12, intercept = 0.41, col = "black", lwd = 0.4, lty = 2) + 
@@ -36,12 +86,63 @@ bat_data_plot %>% ggplot(aes(x = D, y = theta_vl, col = interaction(status, diet
   labs(col = "group", y = expression(theta), x = "standardised phylogenetic distance") + 
   scale_color_manual(values = colors) +
   theme_light() + theme(legend.position = "none")
+######
 
-## Bird plot ####
+
+## Bird plot FIG 3 ####
 
 tax = "bird"
+data_plot <- stan_data_fun(filter(contables, taxon == tax), medn = FALSE)[[2]]
+bat_data_plot <- as.data.frame(bird_winner) %>% colMeans() %>% 
+  data.frame(names(.)) %>% setNames(c("value", "col")) %>% 
+  filter(grepl('theta', col)) %>% separate(col, into=c("name", "index"), sep = 8) %>% 
+  pivot_wider() %>% cbind(bird_data) %>% full_join(data_plot) %>% 
+  mutate(diet.match = recode(diet.match, "different" = "control", "same" = "intraguild"), 
+         status = recode(status, "unaltered" = "intact"))
+
+# bird plot
+linefit <- expand.grid(status = c("intact", "altered"), diet.match = c("intraguild", "control")) %>% 
+  mutate(intercept = c(0.24, 0.41, 0.24, 0.41), 
+         slope  = c(-0.18, -0.18, -0.02, -0.02))
+
+bird_data_plot %>% ggplot(aes(x = D, y = theta_vl, col = interaction(status, diet.match))) + 
+  geom_jitter(alpha = 0.3, width = 0.1) + 
+  geom_ribbon(data = CI_df_bird, aes(x = D, ymin = theta_025, ymax = theta_975), inherit.aes = FALSE, fill = "gray50", alpha = 0.25) +
+  geom_ribbon(data = filter(CI_df_bird, D95q), aes(x = D, ymin = theta_025, ymax = theta_975), fill = "yellow", alpha = 0.4, inherit.aes = FALSE) +
+  geom_abline(data = linefit, aes(intercept = intercept, slope = slope), lty = 2,lwd = 0.4) +
+  geom_ribbon(data = filter(CI_df_bird, D95q), aes(x = D, ymin = theta_mu, ymax = theta_mu), col = "black", inherit.aes = FALSE) +
+  facet_grid(status~diet.match) + labs(col = "group") + 
+  labs(col = "group", y = expression(theta), x = "standardised phylogenetic distance") + 
+  scale_color_manual(values = colors) +
+  theme_light() + theme(legend.position = "none")
+
 
 ######
+
+## SD plot FIG 4 ----
+sd <- list(bat_winner, bat_winner_NT, bird_winner, bird_winner_NT) %>% 
+  map(~data.frame(summary(.x)$random) %>% select(1:4) %>% 
+        setNames(c("Estimate", "SE", "l-95CI", "u-95CI")) %>% rownames_to_column()) %>% 
+  setNames(c("bat_full", "bat_NT", "bird_full", "bird_NT")) %>% 
+  bind_rows(.id = "model") %>% 
+  separate(model, into = c("taxon", "model"), sep = "_") %>% 
+  mutate(status = ifelse(grepl("unaltered", rowname), "intact", "altered"), 
+         diet.match = ifelse(grepl("same", rowname), "intraguild", "control")) %>%
+  rbind(filter(.,taxon == "bird", diet.match == "control") %>% 
+          mutate(diet.match = "intraguild")) %>% select(-rowname)
+
+sd %>% ggplot(aes(x = interaction(status, diet.match), y = Estimate, fill = interaction(status, diet.match))) + 
+  geom_bar(stat = "identity", position = "dodge") + 
+  geom_errorbar(aes(ymin = `l-95CI`, ymax = `u-95CI`), width = 0.1, lwd = 0.5) +
+  facet_grid(model~taxon) + 
+  theme_light() + labs(fill = "group") +
+  scale_fill_manual(values = colors) + 
+  coord_flip(ylim = c(0.3, 0.81)) + 
+  theme(legend.position = "none") + 
+  labs(x = "", y = paste('Standard deviation of', expression(theta)))
+# End SD plot ----
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 ##  SUPPLEMENT 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -91,106 +192,38 @@ ord <- ord %>% ggplot(aes(x = X1, y = X2, col = status)) + geom_point() +
 
 
 ######
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## OLD STUFF ----
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## specs 
-colors <- c("#FFCB5C", "#FF791F", "#2F99DC", "#084887")
-specs <- list(geom_density(lwd = .8, alpha = .3),
-              facet_wrap(parameter~., scales = "free_x"), 
-              scale_color_manual(values = colors), 
-              scale_fill_manual(values = colors))
+## Model checks Figs S6-S8 -----
+## normality check
+p1 <- bird_data_plot %>% ggplot(aes(x = theta_vl)) + geom_histogram() + facet_grid(status~diet.match)
+p2 <- bat_data_plot %>% ggplot(aes(x = theta_vl)) + geom_histogram() + facet_grid(status~diet.match)
+plot_grid(p2, p1, labels = c("A", "B"), ncol = 2)
 
-# FIGURE 2: bat results#
-load("./Results/Omega/interaction/out_jagsfit_bat_medianTRUE_100reps_interaction_baggingFALSE.RData")
-a1 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs 
-load("./Results/Omega/interaction/out_jagsfit_bat_medianFALSE_100reps_interaction_baggingFALSE.RData")
-a2 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
-load("./Results/Omega/interaction/out_jagsfit_bat_medianTRUE_100reps_interaction_baggingTRUE.RData")
-a3 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
-load("./Results/Omega/interaction/out_jagsfit_bat_medianFALSE_100reps_interaction_baggingTRUE.RData")
-a4 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
+## bat bias check
+p1 <- bat_data_plot %>% ggplot(aes(x = log(N_sb), y = sqrt(abs(theta_vl-theta_mu)))) + 
+  geom_point(size = 0.8, alpha = 0.3) + geom_smooth(method = "loess") + facet_grid(status~diet.match) + 
+  labs(y = expression(paste(sqrt(abs(theta-bar(theta))))), 
+       x = expression(ln(N[set])))
 
-legend <- get_legend(a1+ theme(legend.box.margin = margin(0, 0, 0, 1)))
+p2 <- bat_data_plot %>% ggplot(aes(x = log(occ1), y = sqrt(abs(theta_vl-theta_mu)))) + 
+  geom_point(size = 0.8, alpha = 0.4) + geom_smooth(method = "loess") + facet_grid(status~diet.match) + 
+  labs(y = expression(paste(sqrt(abs(theta-bar(theta))))), 
+       x = "logged occupancy of rarer species in pair")
+plot_grid(p2, p1, labels = c("A", "B"), ncol = 2)
 
-p1 <- plot_grid(a1 + theme(legend.position = "none"), a2, a3, a4, align = "hv", nrow = 2)
-plot_grid(p1, legend, nrow = 1, rel_widths = c(2, 0.5))
+## bird bias check
+p1 <- bird_data_plot %>% slice_sample(n = 100000) %>% ggplot(aes(x = log(N_sb), y = sqrt(abs(theta_vl-theta_mu)))) + 
+  geom_point(size = 0.8, alpha = 0.3) + geom_smooth(method = "loess") + facet_grid(status~diet.match) + 
+  labs(y = expression(paste(sqrt(abs(theta-bar(theta))))), 
+       x = expression(ln(N[set])))
 
-# FIGURE 3: bird results #
-load("./Results/Omega/interaction/out_jagsfit_bird_medianTRUE_100reps_interaction_baggingFALSE.RData")
-r1 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs
-load("./Results/Omega/interaction/out_jagsfit_bird_medianFALSE_100reps_interaction_baggingFALSE.RData")
-r2 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
-load("./Results/Omega/interaction/out_jagsfit_bird_medianTRUE_100reps_interaction_baggingTRUE.RData")
-r3 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
-load("./Results/Omega/interaction/out_jagsfit_bird_medianFALSE_100reps_interaction_baggingTRUE.RData")
-r4 <- ggplot(out %>% filter(!parameter %in% c("deviance")), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  specs + theme(legend.position = "none")
+p2 <- bird_data_plot %>% slice_sample(n = 50000) %>% ggplot(aes(x = log(occ1), y = sqrt(abs(theta_vl-theta_mu)))) + 
+  geom_point(size = 0.8, alpha = 0.4) + geom_smooth(method = "loess") + facet_grid(status~diet.match) + 
+  labs(y = expression(paste(sqrt(abs(theta-bar(theta))))), 
+       x = "logged occupancy of rarer species in pair")
+plot_grid(p2, p1, labels = c("A", "B"), ncol = 2)
 
-legend <- get_legend(r1+ theme(legend.box.margin = margin(0, 0, 0, 7)))
-
-p1 <- plot_grid(r1 + theme(legend.position = "none"), r2, r3, r4, align = "hv", nrow = 2)
-plot_grid(p1, legend, nrow = 1, rel_widths = c(2, 0.5))
-
-
-
-# Figs 2 and 3 with extra labels: #
-load("./Results/Omega/interaction/out_jagsfit_bat_medianFALSE_100reps_interaction_baggingFALSE.RData")
-bat_FF <- out
-load("./Results/Omega/interaction/out_jagsfit_bat_medianFALSE_100reps_interaction_baggingTRUE.RData")
-bat_FT <- out
-load("./Results/Omega/interaction/out_jagsfit_bat_medianTRUE_100reps_interaction_baggingFALSE.RData")
-bat_TF <- out
-load("./Results/Omega/interaction/out_jagsfit_bat_medianTRUE_100reps_interaction_baggingTRUE.RData")
-bat_TT <- out
-
-resbat <- bind_rows(list("common_only-bagged" = bat_TT, "common_only-not_bagged" = bat_TF, "all-bagged" = bat_FT, "all-not_bagged" = bat_FF), .id = "model") %>% 
-  separate(model, into = c("species", "bagging"), sep = "-") %>% 
-  mutate(species = factor(species, levels = c("common_only", "all")), 
-         bagging = factor(bagging, levels = c("not_bagged", "bagged")))
-ggplot(resbat %>% filter(parameter != "deviance"), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  geom_density(alpha = 0.25, lwd = 1) + 
-  facet_wrap(bagging~species+parameter, scales = "free", nrow = 2, ncol = 4) +
-  scale_color_manual(values = colors) + scale_fill_manual(values = colors) + ggtitle("Bats")
-
-
-load("./Results/Omega/interaction/out_jagsfit_bird_medianFALSE_100reps_interaction_baggingFALSE.RData")
-bird_FF <- out
-load("./Results/Omega/interaction/out_jagsfit_bird_medianFALSE_100reps_interaction_baggingTRUE.RData")
-bird_FT <- out
-load("./Results/Omega/interaction/out_jagsfit_bird_medianTRUE_100reps_interaction_baggingFALSE.RData")
-bird_TF <- out
-load("./Results/Omega/interaction/out_jagsfit_bird_medianTRUE_100reps_interaction_baggingTRUE.RData")
-bird_TT <- out
-
-resbird <- bind_rows(list("common_only-bagged" = bird_TT, "common_only-not_bagged" = bird_TF, "all-bagged" = bird_FT, "all-not_bagged" = bird_FF), .id = "model") %>% 
-  separate(model, into = c("species", "bagging"), sep = "-") %>% 
-  mutate(species = factor(species, levels = c("common_only", "all")), 
-         bagging = factor(bagging, levels = c("not_bagged", "bagged")))
-
-ggplot(resbird %>% filter(parameter != "deviance"), aes(x = value, col = status_dietmatch, fill = status_dietmatch)) + 
-  geom_density(alpha = 0.25, lwd = 1) + 
-  facet_wrap(bagging~species+parameter, scales = "free", nrow = 2, ncol = 4) +
-  scale_color_manual(values = colors) + scale_fill_manual(values = colors) + ggtitle("Birds")
-
-
-## Figures for CPEG talk 
-out %>% filter(parameter == "omega") %>% 
-  separate(status_dietmatch, c("status", "diet")) %>% 
-  mutate(parameter = recode(parameter, omega = "Co-occurrence"), 
-         status = recode(status, Altered = "Disturbed", Unaltered = "Intact")) %>% 
-  mutate(status = factor(status, levels = c("Intact", "Disturbed")), 
-         diet = factor(diet, levels = c("Same", "Different"))) %>% 
-  ggplot(aes(x = value, col = diet, fill = diet)) + geom_density(alpha = 0.4, lwd = 1) + 
-  facet_grid(status~parameter, scales= "free_x")
+# end model checks----
 
 
 
